b2b02d24e88a80d68c03f2a11abe70c238502fb6
fahim <fahimahamedweb@gmail.com>
2024-02-18 11:03:28 +0600

    spalash screen and data added

--- app/src/main/java/com/example/myshoppal/activities/MainActivity.kt
+++ app/src/main/java/com/example/myshoppal/activities/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.example.myshoppal
+package com.example.myshoppal.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myshoppal.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+       }
 }
+
+
