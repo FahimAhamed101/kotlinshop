fa85ffb75cc135d3091f11b8b8809ebcd5f84318
fahim <fahimahamedweb@gmail.com>
2025-01-02 19:44:09 +0600

    spalash screen added

---
+++ app/src/main/java/com/example/myshoppal/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myshoppal
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }else{
+            @Suppress("DEPRECATION") //find better solution?
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        //Used to change activity, find better solution
+        Handler().postDelayed({//call Handler-Looper to change activity with a 1sec delay
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        },2000)
+    }
+}
